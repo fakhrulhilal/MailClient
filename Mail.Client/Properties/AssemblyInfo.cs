20be9ad38908a4c49c8158310b76c605e31c8588
Fakhrulhilal Maktum <fakhrulhilal@gmail.com>
2018-05-06 16:16:35 +0700

    fix(project): use centralized assembly version

--- Mail.Client/Properties/AssemblyInfo.cs
+++ Mail.Client/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mail MicrosoftClient")]
+[assembly: AssemblyTitle("Tiny Mail Client")]
 [assembly: AssemblyDescription("Simple mail client")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fakhrulhilal Maktum")]
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("34248b85-f868-489f-b4bd-52cb0c0d5a98")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
